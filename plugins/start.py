a2a2d840fdf8340c2f35c1549a6b43e14d9b7437
122344AS <143315452+122344AS@users.noreply.github.com>
2024-07-21 08:06:58 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -84,7 +84,7 @@ async def start_command(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton("⚡️ ᴀʙᴏᴜᴛ", callback_data = "about"),
-                    InlineKeyboardButton('🍁 ᴘʀᴇᴍɪᴜᴍ', url='https://t.me/OtakuFlix_Network/4639')
+                    InlineKeyboardButton('🍁 ᴏᴡɴᴇʀ', url='https://t.me/Shahilakhtar')
                 ]
             ]
                 )
@@ -117,8 +117,8 @@ REPLY_ERROR = """<code>Use this command as a reply to any telegram message with 
 async def not_joined(client: Client, message: Message):
     buttons = [
         [
-            InlineKeyboardButton(text="Join Channel", url=client.invitelink),
-            InlineKeyboardButton(text="Join Channel", url=client.invitelink2),
+            InlineKeyboardButton(text="• Join Channel", url=client.invitelink),
+            InlineKeyboardButton(text="Join Channel •", url=client.invitelink2),
         ]
     ]
     try:
